94ce303572672366d80722f5f56486bd2c09d848
leyuri <leyuri97@gmail.com>
2020-09-23 14:37:55 +0900

    Add exception in addTodo()

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -14,8 +14,15 @@ export default {
     },
     methods: {
         addTodo() {
-            // console.log(this.newItem); //this.newItem의 this는 해당 컴포넌트를 가리킴
-            localStorage.setItem(this.newItem, this.newItem);
+            if (this.newItem !== "") { //input box의 입력값이 있을 때만 저장
+                var value = this.newItem && this.newItem.trim(); //앞뒤 공백 문자열 제거
+                localStorage.setItem(value, value);
+                this.clearInput();
+            }
+            
+        },
+        clearInput() { //inputbox의 입력값 조기화
+            this.newItem = '';
         }
     }
 }
