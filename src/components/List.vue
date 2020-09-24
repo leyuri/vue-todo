3bfb42d84004bd1cacc394439f8a7ef800fdd90c
leyuri <leyuri97@gmail.com>
2020-09-24 10:08:24 +0900

    Add a to-do list display

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,10 +1,25 @@
 <template>
-  <div>list</div>
+  <section>
+    <ul>
+      <li v-for="Item in Items">{{ Item}}</li>
+    </ul>
+  </section>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      Items: []
+    }
+  },
+  created() {
+    if (localStorage.length > 0) {
+      for (var i = 0; i < localStorage.length; i++) {
+        this.Items.push(localStorage.key(i));
+      }
+    }
+  }
 }
 </script>
 
