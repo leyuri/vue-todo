dfad560d8e56e2063f39065f1e03fd88be531931
leyuri <leyuri97@gmail.com>
2020-09-24 14:55:06 +0900

    Add vue animation to add, remove function

--- src/components/List.vue
+++ src/components/List.vue
@@ -1,6 +1,6 @@
 <template>
   <section>
-    <ul>
+    <transition-group name="list" tag="ul">
       <li v-for="(Item, index) in propsdata" :key="Item" class="shadow">
         <i class="checkBtn fas fa-check" aria-hidden="true"></i>
         {{ Item }}
@@ -8,7 +8,7 @@
           <i class="far fa-trash-alt" aria-hidden="true"></i>
         </span>
       </li>
-    </ul>
+    </transition-group>
   </section>
 </template>
 
@@ -52,5 +52,11 @@ export default {
     margin-left: auto;
     color: #de4343;
   }
-
+  .list-enter-active, .list-leave-active {
+    transition: all 1s;
+  }
+  .list-enter, .list-leave-to {
+    opacity: 0;
+    transform: translateY(30px);
+  }
 </style>
